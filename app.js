663150f16f7e757d459a25347faee8368cad5413
France <frascic231@gmail.com>
2025-11-23 01:55:06 +0100

    Enhance app.js for multi-crop functionality
    Refactor app.js to support multiple crops and auto-clickers.

--- app.js
+++ app.js
@@ -99,15 +99,21 @@ function animazioneBottone(button) {
 const app = createApp({
     data() {
       return {
-        contWheat: 0,
+        contMais: 0,
+        contCarrot: 0,
         money: 0,
-        contSell: 0,
-        contAutoClicker: 0,
-        costoUpgrade: [10],
-        costoArea: [500],
-        moltiplicatoreUpgrade: [1.5],
-        livelloUpgrade: [0],
-        autoClickerCost: [100],
+        contSell: [0, 0],
+        contAutoClicker: [0, 0],
+        costoUpgrade: [10, 150],
+        costoArea: [200],
+        moltiplicatoreUpgrade: [1.5, 1.7],
+        livelloUpgrade: [0, 0],
+        autoClickerCost: [100, 220],
+        clickButtons: [],
+        sellButtons: [],
+        upgradeButtons: [],
+        autoClickers: [],
+        textCycle: [],
       }
     },
     
@@ -141,92 +147,136 @@ const app = createApp({
         this.$refs.overlayMap.style.opacity = '0';
         document.getElementById("menuGioco").style.display = "none";
         document.getElementById("strutturaGioco").style.display = "block";
-        this.$refs.primaryButton.style.pointerEvents = 'all';
+        for(let i = 0; i < this.clickButtons.length; i++) {
+          this.clickButtons[i].style.pointerEvents = 'all';
+        }
       },
-      clickType1() {
-        const sellButton = this.$refs.sellButton1;
+      click(num) {
+        const sellButton = this.sellButtons[num];
         sellButton.style.backgroundColor = 'rgba(3, 227, 3, 0.8)';
-        const button = this.$refs.primaryButton;
+        const button = this.clickButtons[num];
         animazioneBottone(button);
-        this.contWheat = Math.round((this.contWheat + 1 + (0.1 * this.livelloUpgrade[0]) ) * 10) / 10;
-        if(this.contWheat == 10) {
-          const sellButton = this.$refs.sellButton1;
-          sellButton.style.opacity = 1;
+        switch(num) {
+          case 0:
+            this.contMais = Math.round((this.contMais + 1 + (0.1 * this.livelloUpgrade[0]) ) * 10) / 10;
+            break;
+          case 1:
+            this.contCarrot = Math.round((this.contCarrot + 1 + (0.2 * this.livelloUpgrade[1]) ) * 10) / 10;
+            break;
+        }
+        if(this.contMais == 10 || this.contCarrot == 10) {
+          sellButton.style.opacity = '1';
           sellButton.style.pointerEvents = 'all';
         }
       },
-      sell() {
-        if(this.contSell == 0) {
-          const textCycle = this.$refs.textCycle1;
-          textCycle.style.opacity = 1;
-          const button = this.$refs.upgradeButton1;
-          if(this.money >= this.costoUpgrade[0]) {
-          const upgradeButton = this.$refs.upgradeButton1;
-          upgradeButton.style.backgroundColor = 'rgba(3, 227, 3, 0.8)';
-        }
-          button.style.opacity = 1;
-          button.style.pointerEvents = 'all';
-        }
-        this.contSell++;
-        moneyEarned = this.contWheat * 2;
-        this.money += Math.round(moneyEarned * 10) / 10;
-        this.contWheat = 0;
-        const button = this.$refs.sellButton1;
-        animazioneBottone(button);
-        button.style.backgroundColor = '#03a909ff';
-        if(this.money >= this.costoUpgrade[0]) {
-          const upgradeButton = this.$refs.upgradeButton1;
-          upgradeButton.style.backgroundColor = 'rgba(3, 227, 3, 0.8)';
-        }
-        if(this.money > 100) {
-          this.$refs.autoClicker1.style.backgroundColor = 'red';
-        }
-      },
-      upgrade(num) {
+      sell(num) {
+        let moneyEarned = 0;
         switch(num) {
           case 0:
-            var button = this.$refs.upgradeButton1;
+            moneyEarned = this.contMais * 2;
+            this.contMais = 0;
+            break;
+          case 1:
+            moneyEarned = this.contCarrot * 3;
+            this.contCarrot = 0;
             break;
         }
+        this.money += Math.round(moneyEarned * 10) / 10;
+        this.money = Number(this.money.toFixed(1));
+        const button = this.sellButtons[num];
+        animazioneBottone(button);
+        button.style.backgroundColor = '#03a909ff';
+        if(this.money >= this.costoUpgrade[num]) {
+          const button = this.upgradeButtons[num];
+          button.style.backgroundColor = 'rgba(3, 227, 3, 0.8)';
+        }
+        if(this.livelloUpgrade[0] >= 6) {
+          if(this.money > 100) {
+            this.autoClickers[0].style.backgroundColor = 'red';
+          } else this.autoClickers[0].style.backgroundColor = 'rgb(166, 2, 2)';
+        } else if(this.livelloUpgrade[1] >= 3) {
+          if(this.money > 220) {
+            this.autoClickers[1].style.backgroundColor = 'red';
+          } else this.autoClickers[1].style.backgroundColor = 'rgb(166, 2, 2)';
+        }
+        if(this.contSell[num] == 0) {
+          const button = this.upgradeButtons[num];
+          const text = this.textCycle[num];
+          text.style.opacity = '1';
+          button.style.opacity = '1';
+          button.style.pointerEvents = 'all';
+        }
+        this.contSell[num]++;
+      },
+      upgrade(num) {
+        const button = this.upgradeButtons[num];
         if(this.money >= this.costoUpgrade[num]) {
           animazioneBottone(button);
           this.money -= Math.round(this.costoUpgrade[num] * 10) / 10;
-          this.money = Number(this.money.toFixed(1))
+          this.money = Number(this.money.toFixed(1));
           this.livelloUpgrade[num]++;
           this.costoUpgrade[num] = Math.floor(this.costoUpgrade[num] * this.moltiplicatoreUpgrade[num]);
           if(this.money < this.costoUpgrade[num]) {
             button.style.backgroundColor = '#03a909ff';
           }
+          if(num == 1 && this.livelloUpgrade[num] == 2) {
+            const button = this.autoClickers[num];
+            button.style.opacity = '1';
+            button.style.pointerEvents = 'all';
+              if(this.money > 100) {
+                button.style.backgroundColor = 'red';
+            }
+          }
           if(this.livelloUpgrade[num] == 6) {
-            const button = this.$refs.autoClicker1;
-            button.style.opacity = 1;
+            const button = this.autoClickers[num];
+            button.style.opacity = '1';
             button.style.pointerEvents = 'all';
             if(this.money > 100) {
               button.style.backgroundColor = 'red';
           }
-          else if(this.livelloUpgrade[num] == 7) {
+        }
+        else if(this.livelloUpgrade[num] == 7) {
             const button = this.$refs.unlockAreaButton;
-            button.style.opacity = 1;
+            button.style.opacity = '1';
             button.style.pointerEvents = 'all';
           }
         }
-        }
         
       },
-      autoClicker(num) {
-        switch(num) {
-          case 0:
-            if(this.money < autoClickerCost[num]) return;
-            setInterval(() => {
-              this.contWheat += this.contAutoClicker;
-            }, 1000);
-            break;
+    autoClicker(num) {  
+        if(this.money < this.autoClickerCost[num]) return;
+        
+        this.contAutoClicker[num]++;
+        if(this.contAutoClicker[num] == 1) {
+          setInterval(() => {
+            if(num == 0) this.contMais += this.contAutoClicker[0];
+            else if(num == 1) this.contCarrot += this.contAutoClicker[1];
+            this.sellButtons[num].style.backgroundColor = 'rgba(3, 227, 3, 0.8)';
+          }, 1000);
         }
-        this.contAutoClicker++;
-        this.money -= autoClickerCost[num];
+        this.money -= this.autoClickerCost[num];
         this.money = Number(this.money.toFixed(1));
-        autoClickerCost[num] += 100;
+        this.autoClickerCost[num] += this.autoClickerCost[num];
+        if(this.money < this.autoClickerCost[num]) {
+          this.autoClickers[num].style.backgroundColor = 'rgb(166, 2, 2)';
+        }
+        if(this.money < this.costoUpgrade[num]) {
+          this.upgradeButtons[num].style.backgroundColor = '#03a909ff';
+        }
     },
+    unlockArea(num) {
+      switch(num) {
+        case 1:
+          if(this.money < this.costoArea[num - 1]) return;
+          this.money -= this.costoArea[num - 1];
+          this.money = Number(this.money.toFixed(1));
+          const button = this.$refs.unlockAreaButton;
+          button.style.display = 'none';
+          const secondArea = this.$refs.secondArea;
+          secondArea.style.display = 'block';
+          break;
+      }
+    }
   }
 });
 
